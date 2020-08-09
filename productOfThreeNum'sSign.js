78d5d7b5d96dbd2187bbb6d8a8a106da24d5b00b
ABIRAMI M <53057686+Abiramimurali@users.noreply.github.com>
2020-08-09 11:37:00 -0700

    Add files via upload
    Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign.
    Sample numbers : 3, -6, 4
    Output : The sign is - 

---
+++ productOfThreeNum'sSign.js
@@ -0,0 +1,18 @@
+var x=3;
+var y=-6;
+var z=4;
+if (x>0 && y>0 && z>0)
+ console.log("The sign is +");
+else if (x>0 && y<0 && z<0)
+ console.log("The sign is +");
+else if (x<0 && y>0 && z<0)
+ console.log("The sign is +");
+else if (x<0 && y<0 && z>0)                          
+ console.log("The sign is +");                     
+else
+ console.log("The sign is -");
+      
+    
+
+
+
