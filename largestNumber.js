77fde26a0771178c78434cb36673693d4462f534
ABIRAMI M <53057686+Abiramimurali@users.noreply.github.com>
2020-08-09 12:10:33 -0700

    Add files via upload

---
+++ largestNumber.js
@@ -0,0 +1,7 @@
+var input1 = 10
+var input2 = 20                        
+if (input1 > input2){                      
+    console.log(input1)}                   
+else {
+    console.log( input2);}
+
