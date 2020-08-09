f1e90c8496b2cdc7e35f78e3110e028afe1610b6
ABIRAMI M <53057686+Abiramimurali@users.noreply.github.com>
2020-08-09 11:33:05 -0700

    Add files via upload
    write a js program using isNaN function

---
+++ isNaNfunction.js
@@ -0,0 +1,10 @@
+//Use of isNaN function? write a js program using isNaN function.
+
+//The isNaN() function determines whether a value is an illegal number (Not-a-Number).If the value is a number then it will return false and the value is not a number then it will return true.
+
+console.log(isNaN("javascript2"))
+console.log(isNaN("200"))             
+console.log(isNaN("20/5"))  
+console.log(isNaN("100E"))  
+console.log(isNaN("Abi"))  
+console.log(isNaN("78.9"))  
